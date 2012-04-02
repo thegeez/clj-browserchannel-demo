1c98efeeb16d9221537c59d241788db2cde1ea7a
Gijs Stuurman <gijsstuurman@gmail.com>
2012-04-02 21:03:23 +0200

    Bump cljs version after nuked jar

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                  [ring/ring-servlet "1.1.0-SNAPSHOT" :exclusions [javax.servlet/servlet-api]]
                  [org.eclipse.jetty/jetty-server "8.1.2.v20120308"];; includes ssl
                  [org.clojure/data.json "0.1.3"]
-                 [org.clojure/clojurescript "0.0-1006" :exclusions [org.clojure/google-closure-library]]
+                 [org.clojure/clojurescript "0.0-1011" :exclusions [org.clojure/google-closure-library]]
                  [net.thegeez/google-closure-library "0.0-1698"]]
   )
