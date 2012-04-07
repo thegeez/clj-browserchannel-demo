5f0b2864a93ec30ae14d94ad2fa8b15c8e133ad3
Gijs Stuurman <gijsstuurman@gmail.com>
2012-04-07 12:29:20 +0200

    remove logging code, new goog closure recognizes presence of console for logging

--- cljs/bc/core.cljs
+++ cljs/bc/core.cljs
@@ -4,12 +4,7 @@
    [goog.net.BrowserChannel :as goog-browserchannel]
    [goog.events :as events]
    [goog.events.KeyCodes :as key-codes]
-   [goog.events.KeyHandler :as key-handler]
-
-   ;;debug
-   [goog.debug.FancyWindow :as debug-window]
-   [goog.net.ChannelDebug :as goog-chandebug]
-   ))
+   [goog.events.KeyHandler :as key-handler]))
 
 (defn handler []
   (let [h (goog.net.BrowserChannel.Handler.)]
@@ -45,15 +40,9 @@
 
 (def channel (goog.net.BrowserChannel.))
 
-;;(def debug-window (doto (goog.debug.FancyWindow. "main")
-;;                    (.setEnabled true)
-;;                    (.init ())))
-
 (defn ^:export run []
   (events/listen js/window "unload" #(.disconnect channel ()))
   (doto channel
-    ;;(.setChannelDebug (goog.net.ChannelDebug.))
     (.setHandler (handler))
-    ;;(.setAllowChunkedMode false)
     (.connect "/channel/test" "/channel/bind")
     ))
